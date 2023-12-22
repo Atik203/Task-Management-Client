8fb596b5f680aac2ae3466bc14d9f6ca5734c4ee
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-12-22 19:43:45 +0600

    fixed responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,23 +13,6 @@ const Home = () => {
     <div className="w-11/12 mx-auto">
       <Banner></Banner>
       <TargetAudienceSection></TargetAudienceSection>
-      {/* <h1 className="text-3xl font-bold text-center mt-28 md:mt-20">
-        Discover Our Popular Contests
-      </h1>
-      <p className="text-center text-base p-4 md:text-xl text-gray-600 mt-4">
-        Immerse yourself in a world of creativity and innovation. <br />{" "}
-        Discover thrilling contests that will challenge your skills and broaden
-        your horizons.
-      </p> */}
-      {/* <div
-        data-aos="fade-up"
-        data-aos-duration="2000"
-        className="my-10 w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center"
-      >
-        {contests?.map((item) => (
-          <ContestCard item={item} key={item._id}></ContestCard>
-        ))}
-      </div> */}
     </div>
   );
 };
